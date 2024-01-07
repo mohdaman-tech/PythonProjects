a88a90a3d5c7eb57b2b1d8e27309e61ab73d730e
Mohd Aman Ullah Khan <amaank43@gmail.com>
2024-01-07 22:08:49 +0530

    Update app.py

--- YTSNTMNT/app.py
+++ YTSNTMNT/app.py
@@ -2,3 +2,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 from flask import Flask,render_template,request
 import time
+
+app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
