d4401f7e091cc79f388ee04aa6392c0a914ba6f3
Mohd Aman Ullah Khan <mohdaman.tech@gmail.com>
2024-11-25 12:53:59 +0530

    Update Game2048.py

--- 2048/Game2048.py
+++ 2048/Game2048.py
@@ -117,4 +117,121 @@ def compress(mat):
     return new_mat, changed
 
 
+# function to merge the cells
+# in matrix after compressing
+def merge(mat):
+    
+    changed = False
+    
+    for i in range(4):
+        for j in range(3):
+
+            # if current cell has same value as
+            # next cell in the row and they
+            # are non empty then
+            if(mat[i][j] == mat[i][j + 1] and mat[i][j] != 0):
+
+                # double current cell value and
+                # empty the next cell
+                mat[i][j] = mat[i][j] * 2
+                mat[i][j + 1] = 0
+
+                # make bool variable True indicating
+                # the new grid after merging is
+                # different.
+                changed = True
+
+    return mat, changed
+
+# function to reverse the matrix
+# means reversing the content of
+# each row (reversing the sequence)
+def reverse(mat):
+    new_mat =[]
+    for i in range(4):
+        new_mat.append([])
+        for j in range(4):
+            new_mat[i].append(mat[i][3 - j])
+    return new_mat
+
+# function to get the transpose
+# of matrix means interchanging
+# rows and column
+def transpose(mat):
+    new_mat = []
+    for i in range(4):
+        new_mat.append([])
+        for j in range(4):
+            new_mat[i].append(mat[j][i])
+    return new_mat
+
+# function to update the matrix
+# if we move / swipe left
+def move_left(grid):
+
+    # first compress the grid
+    new_grid, changed1 = compress(grid)
+
+    # then merge the cells.
+    new_grid, changed2 = merge(new_grid)
+    
+    changed = changed1 or changed2
+
+    # again compress after merging.
+    new_grid, temp = compress(new_grid)
+
+    # return new matrix and bool changed
+    # telling whether the grid is same
+    # or different
+    return new_grid, changed
+
+# function to update the matrix
+# if we move / swipe right
+def move_right(grid):
+
+    # to move right we just reverse
+    # the matrix 
+    new_grid = reverse(grid)
+
+    # then move left
+    new_grid, changed = move_left(new_grid)
+
+    # then again reverse matrix will
+    # give us desired result
+    new_grid = reverse(new_grid)
+    return new_grid, changed
+
+# function to update the matrix
+# if we move / swipe up
+def move_up(grid):
+
+    # to move up we just take
+    # transpose of matrix
+    new_grid = transpose(grid)
+
+    # then move left (calling all
+    # included functions) then
+    new_grid, changed = move_left(new_grid)
+
+    # again take transpose will give
+    # desired results
+    new_grid = transpose(new_grid)
+    return new_grid, changed
+
+# function to update the matrix
+# if we move / swipe down
+def move_down(grid):
+
+    # to move down we take transpose
+    new_grid = transpose(grid)
+
+    # move right and then again
+    new_grid, changed = move_right(new_grid)
+
+    # take transpose will give desired
+    # results.
+    new_grid = transpose(new_grid)
+    return new_grid, changed
+
+#
 
