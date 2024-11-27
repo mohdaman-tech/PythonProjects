6cf5aea9f4a879e83ea5ddf9fd1a9cb4b9d1c178
Mohd Aman Ullah Khan <mohdaman.tech@gmail.com>
2024-11-27 23:33:51 +0530

    Update GuessTheNumber.py

--- GuessTheNumber.py
+++ GuessTheNumber.py
@@ -1,18 +1,17 @@
- 1. # This is a Guess the Number game.
- 2. import random
- 3.
- 4. guessesTaken = 0
- 5.
- 6. print('Hello! What is your name?')
- 7. myName = input()
- 8.
- 9. number = random.randint(1, 20)
-10. print('Well, ' + myName + ', I am thinking of a number between 1 and 20.')
-11.
-12. for guessesTaken in range(6):
-13.     print('Take a guess.') # Four spaces in front of "print"
-14.     guess = input()
-15.     guess = int(guess)
-16.
-17.     if guess < number:
-18.         print('Your guess is too low.') # Eight spaces in front of "print"
+import random
+guessesTaken = 0
+print('Hello! What is your name?')
+myName = input()
+number = random.randint(1, 20)
+print('Well, ' + myName + ', I am thinking of a number between 1 and 20.')
+for guessesTaken in range(6):
+  print('Take a guess.')
+  guess = input()
+  guess = int(guess)
+  if guess < number:
+    print('Your guess is too low.')
+  if guess > number:
+     print('Your guess is too high.')
+  if guess == number:
+     break
+
